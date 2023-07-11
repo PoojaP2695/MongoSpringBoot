55ac2b85f9e433335f1e7786e1014170ea6d4296
PoojaP2695 <pujapatil0303@gmail.com>
2023-07-11 20:45:45 +0530

    [STRNO205] initial commit

---
+++ MongoDBSpringBootEx/src/main/java/com/csi/MongoDbSpringBootExApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSpringBootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbSpringBootExApplication.class, args);
+	}
+
+}
---
+++ MongoDBSpringBootEx/src/main/java/com/csi/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.csi.controller;
+
+import com.csi.exception.RecordNotFoundException;
+import com.csi.model.Employee;
+import com.csi.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeServiceImpl employeeServiceImpl;
+
+    @PostMapping("/savedata")
+    public ResponseEntity<Employee> saveData(@RequestBody Employee employee){
+        return ResponseEntity.ok(employeeServiceImpl.saveData(employee));
+    }
+    @GetMapping("/getdatabyid/{empId}")
+    public ResponseEntity <Optional<Employee>> getDataById(@PathVariable int empId){
+        return ResponseEntity.ok(employeeServiceImpl.getDataById(empId));
+    }
+    @GetMapping("/getalldata")
+    public ResponseEntity<List<Employee>> getAllData(){
+        return ResponseEntity.ok(employeeServiceImpl.getAllData());
+    }
+    @PutMapping("updatedata/{empId}")
+    public ResponseEntity <Employee> updateData(@PathVariable int empId, @RequestBody Employee employee){
+        Employee employee1 = employeeServiceImpl.getDataById(empId).orElseThrow(() -> new RecordNotFoundException("Employee Id Does Not Exits"));
+        employee1.setEmpName(employee.getEmpName());
+        employee1.setEmpSalary(employee.getEmpSalary());
+        employee1.setEmpDOB(employee.getEmpDOB());
+        return ResponseEntity.ok(employeeServiceImpl.updateData(employee1));
+    }
+
+    @GetMapping("/deletedatabyid/{empId}")
+    public ResponseEntity <String> deleteDataById(@PathVariable int empId){
+       employeeServiceImpl.deleteDataById(empId);
+        return ResponseEntity.ok("Data Deleted by Succefully");
+    }
+}
